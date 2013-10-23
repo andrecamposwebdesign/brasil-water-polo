f8cef1fcdb63ed05ed1eb4e71abdf9f38d7cae7b
unknown <andrecampos.webdesign@gmail.com>
2013-10-23 20:23:30 -0200

    eescudos

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+$(document).ready(function() {
+  	$( ".banner, .especialies" ).fadeIn( 2500 );
+});
+
+
+$( "a" ).click(function() {
+  	$( ".banner" ).hide( 1000 );
+});
+
+
+
+
+
+$(window).load(function() {
+	$('.loading').fadeOut();
+});
